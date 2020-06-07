b00fe1fb60ca4fb6cb76703114a7ec65e9d97eec
Jhonas Alves <jhonas.alvesm@gmail.com>
2020-06-07 10:26:14 -0300

    Upload imagem

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import axios from "axios";
 import { LeafletMouseEvent } from "leaflet";
 import api from "../../services/api";
 
+import Dropzone from "../../components/Dropzone";
+
 import "./styles.css";
 
 import logo from "../../assets/logo.svg";
@@ -47,6 +49,7 @@ const CreatePoint = () => {
     0,
     0,
   ]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -130,24 +133,24 @@ const CreatePoint = () => {
     const { name, email, whatsapp } = formData;
     const uf = selectedUf;
     const city = selectedCity;
-    const [latidude, longitude] = selectedPosition;
+    const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latidude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
 
-    console.log(data);
-    // await api.post('points', data)
+    if (selectedFile) data.append("image", selectedFile);
 
-    // alert('Ponto de coleta criado.')
+    await api.post("points", data);
+
+    alert("Ponto de coleta criado.");
     history.push("/");
   }
 
@@ -166,6 +169,8 @@ const CreatePoint = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
